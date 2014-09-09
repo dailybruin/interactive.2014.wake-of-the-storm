18852e8ac4f89e07f4b355545c9c1e64426a60b8
Byron Lutz <byronlutz@gmail.com>
2014-09-10 00:26:39 +0800

    Changed map to support geojson from this repo and made current location pop up by default

--- js/current-location.js
+++ js/current-location.js
@@ -1,7 +1,30 @@
+---
+---
+var CURRENT_MARKER_COLOR = "#f86767";
 $(document).ready(function(){
     L.mapbox.accessToken = "pk.eyJ1IjoiZGJvbmxpbmUiLCJhIjoiNmtQLUNuVSJ9.NH129qs3Mu-tWPspZuuOuQ"
-    window.map = L.mapbox.map('current-location-map', 'dbonline.jea97dih');
-    map.on('ready', function(){
-        map.setView([14.083, 124.239], 6)
+    window.map = L.mapbox.map('current-location-map', 'dbonline.jf680m39', {
+        zoom: 6,
+        center: [14.083, 124.239],
+        worldCopyJump: true
     });
+    window.featureLayer = L.mapbox.featureLayer()
+        .addTo(map);
+
+    featureLayer.on("layeradd", function(e) {
+        try {
+            var layer = e.layer;
+            var props = e.layer.feature.properties;
+            if (e.layer.feature.properties == undefined)
+                return;
+        } catch (err) {
+            return;
+        }
+
+        if (props['marker-color'] == CURRENT_MARKER_COLOR) {
+            layer.togglePopup();
+        }
+    });
+
+    featureLayer.loadURL("{{site.url}}/assets/geo/current-location.geojson");
 });
