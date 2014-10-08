475973630cb432b15e934b09e931828e3cc4b0aa
Byron Lutz <byronlutz@gmail.com>
2014-10-08 05:24:44 -0700

    Added header to mass grave

--- js/grave.js
+++ js/grave.js
@@ -3,8 +3,58 @@
 (function() {
 $.getJSON("{{ site.url }}/assets/san-joaquin.json", null, function(data) { 
     ORIGINAL_DATA = data;
-    draw()
+    $(document).ready(function(){
+        draw();
+        initBanner(data);
+    });
 });
+
+function initBanner(data) {
+    var el = $("#grave-cover-back");
+    var FAKE_COVER_HEIGHT = 620 + 250;
+    function fillCover() {
+        var i = 0;
+        var last = el.find(".grave-image").last();
+        if(last.length > 0 && last.position().top > FAKE_COVER_HEIGHT) {
+            return;
+        }
+        while(true && i < 100) {
+            i += 1;
+            var index = parseInt(Math.random() * data.length);
+            var individualImage = data[index].image;
+            if(individualImage == undefined || individualImage == "") {
+                continue;
+            }
+            var newElement = $("<div class='grave-image' style='background-image: url({{ site.url }}/assets/images/grave/thumbs/"+individualImage+".jpg)' />")
+            el.append(newElement);
+            if(newElement.position().top > FAKE_COVER_HEIGHT) {
+                break;
+            }
+        }
+    }
+    fillCover();
+
+    function tick() {
+        var id = window.requestAnimationFrame(tick);
+        var newPosition = el.position().top- 0.25
+        if(newPosition <= -150) {
+            newPosition = 0;
+            var toRemove = [];
+            el.children().each(function() {
+                if($(this).position().top == 0) {
+                    toRemove.push(this);
+                }
+            });
+            for(var i = 0; i < toRemove.length; i++) {
+                toRemove[i].remove();
+            }
+            fillCover();
+        }
+        el.css("top", newPosition + "px");
+    }
+    requestAnimationFrame(tick);
+}
+
 
 var TYPHOON_DATE = moment("November 8, 2013");
 var TYPHOON_DATE_FORMATTED = moment("November 8, 2013").format("MMM. D, YYYY");
@@ -32,7 +82,7 @@ function getIndividual(record) {
         cont.prepend("<a href='{{site.url}}/assets/images/grave/"+record.image+".jpg'><img src='{{ site.url }}/assets/images/grave/thumbs/"+record.image+".jpg'></a>");
     }
     else {
-        cont.prepend("<img src='{{ site.url }}/assets/images/grave/unmarked.jpg'>");
+        cont.prepend("<img src='{{ site.url }}/assets/images/grave/thumbs/unmarked.jpg'>");
     }
     el.append("<span class='name'>"+record.name+"</span>");
     if(record.dob != undefined && record.dob != "") {
